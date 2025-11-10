6705537f31828f4ffb02c5df7b941010069b14e9
hawi-me <hawiabdi41@gmail.com>
2025-11-11 00:54:57 +0300

    small fix

--- e-shop/src/app/(authenticated)/page.tsx
+++ e-shop/src/app/(authenticated)/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState, useEffect, useRef, useCallback } from "react"
 import { ProductCard } from "../../../components/product-card"
 import { SearchBar } from "../../../components/search-bar"
